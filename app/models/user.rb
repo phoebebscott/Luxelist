f5df8482ff79d32fe50ea87225608e13cfb11a7e
Mary Keenan <marykeenan@Marys-MacBook-Pro.local>
2014-05-29 16:30:48 -0700

    completed six user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+validates_presence_of :first_name, :email, :password
+validates_uniqueness_of :email
+validates :password, length: {minimum: 6}
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+    it "is a valid user that has a first name, email and password" do
+	  	person = User.new(first_name: "Jane", email: "jane@doe.com", password: "123456")
+	  	expect(person).to be_valid
+  	end
+
+  	it "is not a valid user without a name" do
+  		person = User.new(email: "jane@doe.com", password: "123456")
+  		expect(person).to be_invalid
+  	end
+
+  	it "is not a valid user without an email address" do
+  		person = User.new(first_name: "Jane", password: "123456")
+  		expect(person).to be_invalid
+  	end
+
+  	it "is not a valid user without a password" do
+  		person = User.new(first_name: "Jane", email: "jane@doe.com")
+  		expect(person).to be_invalid
+  	end
+
+  	it "is not a valid user without a unique email address" do
+  		person = User.new(email: "jane@doe.com")
+  		person2 = User.new(email: "jane@doe.com")
+  		expect(person2).to be_invalid
+  	end
+
+  	it "is only a valid user if password length is at least six characters" do
+  		person = User.new(first_name: "Jane", email: "jane@doe.com", password: "1234")
+  		expect(person).to be_invalid
+  	end
+
+
 end
