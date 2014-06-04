95c5a088755d6b79862d0cd916475d766b252ff6
fooeyandnuts <william.sipes@gmail.com>
2014-06-04 13:40:27 -0700

    phoebes changes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,32 @@
 class ItemsController < ApplicationController
+	before_action :set_item
 
-def index
+	def index
+		@items = Items.party(params[:heading])
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
 end
 
-def show
-end
-
-def new
-end
-
-def create
-end
-
-def edit
-end
-
-def update
-end
-
-def destroy
-end
-
-end
+private
+# Use callbacks to share common setup or constraints between actions.
+	def set_item
+	  @item = Item.find(params[:id])
+	end
