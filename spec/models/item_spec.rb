f5e0acf2aaa1b79158c3c9de275a88a938455531
nestolobo <ernest.wolfe@gmail.com>
2014-06-06 10:01:06 -0700

    met minimum model tests requirement

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 
 describe Item do
-  	it "is not a valid item without a title" do
-  		item = Item.new(price: 100, location: "US-CA-LAX")
-  		expect(item).to be_invalid
+	it "is not a valid item without a title" do
+		item = Item.new(price: 100, location: "US-CA-LAX")
+		expect(item).to be_invalid
   end
 
-    it "is not a valid item without a location" do
-    	item = Item.new(title: "sofa", price: 100)
-  		expect(item).to be_invalid
+  it "is not a valid item without a location" do
+  	item = Item.new(title: "sofa", price: 100)
+		expect(item).to be_invalid
   end
 
-  	it "is not a valid item without a price" do
-  		item = Item.new(title: "sofa", location: "US-CA-LAX")
-  		expect(item).to be_invalid
+	it "is not a valid item without a price" do
+		item = Item.new(title: "sofa", location: "US-CA-LAX")
+		expect(item).to be_invalid
 	end
+  it { should have_many(:favorites)}
+  it { should have_many(:users)}
 end
